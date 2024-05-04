f89a3fe50f31c975c2b2359f60cec8a3811de871
ADARSH SAJWAN <95115993+GE210111034@users.noreply.github.com>
2024-05-04 18:57:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Create a new VNC password file
-mkdir -p /root/.vnc
-echo "$VNC_PW" | vncpasswd -f > /root/.vnc/passwd
-chmod 600 /root/.vnc/passwd
+# Start the X virtual framebuffer
+Xvfb :99 &
 
-# Start the VNC server
-vncserver -geometry 1920x1080 :1 -localhost no -alwaysshared -depth 24 &
+# Start Fluxbox window manager
+fluxbox &
 
-# Start the Xfce desktop in virtual framebuffer mode
-Xvfb :1 -ac -screen 0 1920x1080x24 & DISPLAY=:1 startxfce4 &
+# Start x11vnc server
+x11vnc -forever -shared -rfbauth ~/.vnc/passwd -display :99 -rfbport 5900 &
 
 # Keep the container running
-read -p "VNC server is running. Press any key to exit..." -n1 -s
-
-# Stop the VNC server
-vncserver -kill :1
+tail -f /dev/null
